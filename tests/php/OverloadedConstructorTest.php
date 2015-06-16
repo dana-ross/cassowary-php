3f7910f7b85b608c17ad7b9c21186bbf5e02abc2
Dave Ross <dave@davidmichaelross.com>
2015-06-16 18:04:57 -0400

    Fixed unit tests for new constructor overloading

--- tests/php/OverloadedConstructorTest.php
+++ tests/php/OverloadedConstructorTest.php
@@ -4,23 +4,23 @@ class TestOverloadedMethods extends PHPUnit_Framework_TestCase {
 
 	function test_constructor_name() {
 
-		$this->assertEquals( '__construct_string', TestOverloadedMethodsA::_constructor_name( array( 'test' ) ) );
-		$this->assertEquals( '__construct_integer', TestOverloadedMethodsA::_constructor_name( array( 1 ) ) );
-		$this->assertEquals( '__construct_double', TestOverloadedMethodsA::_constructor_name( array( 1.0 ) ) );
-		$this->assertEquals( '__construct_array', TestOverloadedMethodsA::_constructor_name( array(
+		$this->assertEquals( '__construct_string', TestOverloadedMethodsA::_overridden_impl_fn_name( '__construct', array( 'test' ) ) );
+		$this->assertEquals( '__construct_integer', TestOverloadedMethodsA::_overridden_impl_fn_name( '__construct', array( 1 ) ) );
+		$this->assertEquals( '__construct_double', TestOverloadedMethodsA::_overridden_impl_fn_name( '__construct', array( 1.0 ) ) );
+		$this->assertEquals( '__construct_array', TestOverloadedMethodsA::_overridden_impl_fn_name( '__construct', array(
 			array(
 				1,
 				2,
 				3
 			)
 		) ) );
-		$this->assertEquals( '__construct_object', TestOverloadedMethodsA::_constructor_name( array( new stdClass() ) ) );
+		$this->assertEquals( '__construct_stdclass', TestOverloadedMethodsA::_overridden_impl_fn_name( '__construct', array( new stdClass() ) ) );
 
-		$this->assertEquals( '__construct_string_integer', TestOverloadedMethodsA::_constructor_name( array(
+		$this->assertEquals( '__construct_string_integer', TestOverloadedMethodsA::_overridden_impl_fn_name( '__construct', array(
 			'test',
 			5
 		) ) );
-		$this->assertEquals( '__construct_string_object', TestOverloadedMethodsA::_constructor_name( array(
+		$this->assertEquals( '__construct_string_stdclass', TestOverloadedMethodsA::_overridden_impl_fn_name( '__construct', array(
 			'test',
 			new stdClass()
 		) ) );
@@ -48,8 +48,14 @@ class TestOverloadedMethods extends PHPUnit_Framework_TestCase {
 	}
 
 	function test_overriden_impl_fn_name() {
-		$this->assertEquals('example_double_integer', TestOverloadedMethodsA::_overridden_impl_fn_name('example', array(5.5, 5)));
-		$this->assertEquals('example_testoverloadedmethodsa_integer', TestOverloadedMethodsA::_overridden_impl_fn_name('example', array(new TestOverloadedMethodsA('test'), 5)));
+		$this->assertEquals( 'example_double_integer', TestOverloadedMethodsA::_overridden_impl_fn_name( 'example', array(
+			5.5,
+			5
+		) ) );
+		$this->assertEquals( 'example_testoverloadedmethodsa_integer', TestOverloadedMethodsA::_overridden_impl_fn_name( 'example', array(
+			new TestOverloadedMethodsA( 'test' ),
+			5
+		) ) );
 	}
 
 }
