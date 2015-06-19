97630d89d56086d42d7e907373d8dc240a704ea0
Dave Ross <dave@davidmichaelross.com>
2015-06-18 20:38:26 -0400

    @covers annotations

--- tests/php/TimerTest.php
+++ tests/php/TimerTest.php
@@ -2,12 +2,20 @@
 
 class TimerTest extends PHPUnit_Framework_TestCase {
 
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\Timer::Start
+	 * @covers \DaveRoss\CassowaryConstraintSolver\Timer::IsRunning
+	 */
 	function test_is_running() {
 		$timer = new \DaveRoss\CassowaryConstraintSolver\Timer();
 		$timer->Start();
 		$this->assertEquals( true, $timer->IsRunning() );
 	}
 
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\Timer::Stop
+	 * @covers \DaveRoss\CassowaryConstraintSolver\Timer::ElapsedTime
+	 */
 	function test_elapsed_time() {
 		$timer = new \DaveRoss\CassowaryConstraintSolver\Timer();
 		$timer->Start();
@@ -16,6 +24,9 @@ class TimerTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( 2, floor( $timer->ElapsedTime() ) );
 	}
 
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\Timer::Reset
+	 */
 	function test_reset() {
 		$timer = new \DaveRoss\CassowaryConstraintSolver\Timer();
 		$timer->Start();
