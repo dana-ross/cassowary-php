bbf24dfb7892aff732e042ffbf5f679678817abb
Dave Ross <dave@davidmichaelross.com>
2015-06-22 08:48:52 -0400

    More ClLinearExpression unit tests

--- tests/php/ClLinearExpressionTest.php
+++ tests/php/ClLinearExpressionTest.php
@@ -76,20 +76,32 @@ class ClLinearExpressionTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function test_clone() {
+		$key     = new \DaveRoss\CassowaryConstraintSolver\ClDummyVariable();
 		$example = new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression(
-			new \DaveRoss\CassowaryConstraintSolver\ClDummyVariable(),
+			$key,
 			1.0,
 			2.0
 		);
-		$clone = clone($example);
+		$clone   = clone( $example );
 		$this->assertInstanceOf( '\DaveRoss\CassowaryConstraintSolver\ClLinearExpression', $clone );
-		foreach ( $example->terms() as $index => $key ) {
-			// Should only be one item in this array
-			$this->assertEquals( 0, $index );
-			$this->assertInternalType( 'double', $example->terms()[ $key ]->doubleValue() );
-			$this->assertEquals( 2.0, $example->terms()[ $key ]->doubleValue() );
-		}
+		$this->assertEquals( 1, count( $example->terms()->size() ) );
+		$this->assertInternalType( 'double', $example->terms()[ $key ]->doubleValue() );
+		$this->assertEquals( 1.0, $example->terms()[ $key ]->doubleValue() );
+	}
 
+	public function test_times_double() {
+		$key                = new \DaveRoss\CassowaryConstraintSolver\ClDummyVariable();
+		$example            = new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression(
+			$key,
+			1.0,
+			2.0
+		);
+		$multiplied_example = $example->multiplyMe( 2.0 );
+		$this->assertInstanceOf( '\DaveRoss\CassowaryConstraintSolver\ClLinearExpression', $multiplied_example );
+		$this->assertSame( $example, $multiplied_example );
+		$this->assertEquals( 1, count( $example->terms()->size() ) );
+		$this->assertInternalType( 'double', $example->terms()[ $key ]->doubleValue() );
+		$this->assertEquals( 2.0, $example->terms()[ $key ]->doubleValue() );
 	}
 
 }
