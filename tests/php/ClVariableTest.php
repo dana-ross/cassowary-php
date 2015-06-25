d95ee838d39a07e30544b98f5a11090faf603184
Dave Ross <dave@davidmichaelross.com>
2015-06-24 21:49:52 -0400

    ClVariable unit tests

--- tests/php/ClVariableTest.php
+++ tests/php/ClVariableTest.php
@@ -3,6 +3,15 @@
 class ClVariableTest extends \PHPUnit_Framework_TestCase {
 
 	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::__construct_default
+	 */
+	function test_construct_default() {
+		$clVariable = new \DaveRoss\CassowaryConstraintSolver\ClVariable();
+		$this->assertInternalType( 'double', $clVariable->getValue() );
+		$this->assertEquals( 0.0, $clVariable->getValue() );
+	}
+
+	/**
 	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::__construct_string
 	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::__toString
 	 */
@@ -34,12 +43,28 @@ class ClVariableTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( 6.0, $clVariable->getValue() );
 
 		// Test type coercion
-		$clVariable->setValue(5);
+		$clVariable->change_value( 5 );
 		$this->assertInternalType( 'double', $clVariable->getValue() );
 		$this->assertEquals( 5.0, $clVariable->getValue() );
 
 	}
 
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::set_value
+	 */
+	function test_set_value() {
+
+		$clVariable = new \DaveRoss\CassowaryConstraintSolver\ClVariable( 5.0 );
+		$clVariable->change_value( 6.0 );
+		$this->assertInternalType( 'double', $clVariable->getValue() );
+		$this->assertEquals( 6.0, $clVariable->getValue() );
+
+	}
+
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::setAttachedObject
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::getAttachedObject
+	 */
 	function test_attached_object() {
 
 		$clVariable = new \DaveRoss\CassowaryConstraintSolver\ClVariable( 5.0 );
@@ -49,4 +74,41 @@ class ClVariableTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($obj, $clVariable->getAttachedObject());
 
 	}
+
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::isRestricted
+	 */
+	function test_is_restricted() {
+		$clVariable = new \DaveRoss\CassowaryConstraintSolver\ClVariable( 5.0 );
+		$this->assertInternalType( 'boolean', $clVariable->isRestricted() );
+		$this->assertEquals( false, $clVariable->isRestricted() );
+	}
+
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::isPivotable
+	 */
+	function test_is_pivotable() {
+		$clVariable = new \DaveRoss\CassowaryConstraintSolver\ClVariable( 5.0 );
+		$this->assertInternalType( 'boolean', $clVariable->isPivotable() );
+		$this->assertEquals( false, $clVariable->isPivotable() );
+	}
+
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::isExternal
+	 */
+	function test_is_external() {
+		$clVariable = new \DaveRoss\CassowaryConstraintSolver\ClVariable( 5.0 );
+		$this->assertInternalType( 'boolean', $clVariable->isExternal() );
+		$this->assertEquals( true, $clVariable->isExternal() );
+	}
+
+	/**
+	 * @covers \DaveRoss\CassowaryConstraintSolver\ClVariable::isDummy
+	 */
+	function test_is_dummy() {
+		$clVariable = new \DaveRoss\CassowaryConstraintSolver\ClVariable( 5.0 );
+		$this->assertInternalType( 'boolean', $clVariable->isDummy() );
+		$this->assertEquals( false, $clVariable->isDummy() );
+	}
+
 }
