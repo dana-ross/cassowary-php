557af64345bb7c6c282a50a853847a3c16649b98
Dave Ross <dave@davidmichaelross.com>
2015-07-09 09:05:16 -0400

    More ClLinearEquation unit tests

--- tests/php/clconstraint/ClLinearEquationTest.php
+++ tests/php/clconstraint/ClLinearEquationTest.php
@@ -8,10 +8,14 @@ class ClLinearEquationTest extends PHPUnit_Framework_TestCase {
 	 * @covers \DaveRoss\CassowaryConstraintSolver\ClConstraint::weight
 	 * @covers \DaveRoss\CassowaryConstraintSolver\ClLinearConstraint::expression
 	 */
-	public function test_construct_cllinearexpression_cllinearexpression_clstrength_double() {
+	public function test_construct_cllinearexpression_clstrength_double() {
 
 		$expression = new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression( 5.0 );
-		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation( $expression, \DaveRoss\CassowaryConstraintSolver\ClStrength::$medium, 5.0 );
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			$expression,
+			\DaveRoss\CassowaryConstraintSolver\ClStrength::$medium,
+			5.0
+		);
 		$this->assertInstanceOf( '\DaveRoss\CassowaryConstraintSolver\ClStrength', $constraint->strength() );
 		$this->assertSame( \DaveRoss\CassowaryConstraintSolver\ClStrength::$medium, $constraint->strength() );
 		$this->assertInternalType( 'double', $constraint->weight() );
@@ -58,6 +62,179 @@ class ClLinearEquationTest extends PHPUnit_Framework_TestCase {
 		$this->assertSame( $expression, $constraint->expression() );
 
 	}
+
+	public function test_construct_clabstractvariable_cllinearexpression_clstrength_double() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' ),
+			new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression( 3.0 ),
+			\DaveRoss\CassowaryConstraintSolver\ClStrength::$medium,
+			4.0
+		);
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
+	public function test_construct_clabstractvariable_cllinearexpression() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' ),
+			new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression( 3.0 )
+		);
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
+	public function test_construct_clabstractvariable_double_clstrength_double() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' ),
+			2.0,
+			\DaveRoss\CassowaryConstraintSolver\ClStrength::$weak,
+			4.0
+		);
+
+		$this->assertEquals(4.0, $constraint->weight());
+		$this->assertSame(\DaveRoss\CassowaryConstraintSolver\ClStrength::$weak, $constraint->strength());
+
+		$expression = $constraint->expression();
+		$this->assertEquals(2.0, $expression->constant());
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
+	public function test_construct_clabstractvariable_double_clstrength() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' ),
+			2.0,
+			\DaveRoss\CassowaryConstraintSolver\ClStrength::$weak
+		);
+
+		$this->assertEquals(1.0, $constraint->weight());
+		$this->assertSame(\DaveRoss\CassowaryConstraintSolver\ClStrength::$weak, $constraint->strength());
+
+		$expression = $constraint->expression();
+		$this->assertEquals(2.0, $expression->constant());
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
+	public function test_construct_clabstractvariable_double() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' ),
+			2.0
+		);
+
+		$this->assertEquals(1.0, $constraint->weight());
+		$this->assertSame(\DaveRoss\CassowaryConstraintSolver\ClStrength::$required, $constraint->strength());
+
+		$expression = $constraint->expression();
+		$this->assertEquals(2.0, $expression->constant());
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
+	public function test_construct_cllinearexpression_clabstractvariable_clstrength_double() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression( 5.0 ),
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' ),
+			\DaveRoss\CassowaryConstraintSolver\ClStrength::$medium,
+			2.0
+		);
+
+		$this->assertEquals(2.0, $constraint->weight());
+		$this->assertSame(\DaveRoss\CassowaryConstraintSolver\ClStrength::$medium, $constraint->strength());
+
+		$expression = $constraint->expression();
+		$this->assertEquals(5.0, $expression->constant());
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
+	public function test_construct_cllinearexpression_clabstractvariable_clstrength() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression( 5.0 ),
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' ),
+			\DaveRoss\CassowaryConstraintSolver\ClStrength::$medium
+		);
+
+		$this->assertEquals(1.0, $constraint->weight());
+		$this->assertSame(\DaveRoss\CassowaryConstraintSolver\ClStrength::$medium, $constraint->strength());
+
+		$expression = $constraint->expression();
+		$this->assertEquals(5.0, $expression->constant());
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
+	public function test_construct_cllinearexpression_clabstractvariable() {
+
+		$constraint = new \DaveRoss\CassowaryConstraintSolver\ClLinearEquation(
+			new \DaveRoss\CassowaryConstraintSolver\ClLinearExpression( 5.0 ),
+			new \DaveRoss\CassowaryConstraintSolver\ClObjectiveVariable( 'example' )
+		);
+
+		$this->assertEquals(1.0, $constraint->weight());
+		$this->assertSame(\DaveRoss\CassowaryConstraintSolver\ClStrength::$required, $constraint->strength());
+
+		$expression = $constraint->expression();
+		$this->assertEquals(5.0, $expression->constant());
+
+		$terms = $constraint->expression()->terms();
+		$this->assertEquals( 1, $terms->keySet_iterator()->getSize() );
+		$variable = $terms->keySet_iterator()->current();
+		$this->assertEquals( 'example', $variable->name() );
+		$value = $terms[ $variable ];
+		$this->assertEquals( - 1.0, $value->doubleValue() );
+
+	}
+
 
 	/**
 	 * @covers \DaveRoss\CassowaryConstraintSolver\ClLinearEquation::__toString
