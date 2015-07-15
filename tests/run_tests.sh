160a53d4a399b19363b45b73dd112b0ce1318c86
Dave Ross <dave@davidmichaelross.com>
2015-07-15 01:33:31 -0400

    Trying an improved travis-ci config which should fix issue where hhvm & php 7 sent bad coverage data https://twitter.com/scrutinizerci/status/592690296317681665

--- tests/run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-phpunit --configuration phpunit.xml --coverage-clover=coverage.clover
-wget https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --format=php-clover coverage.clover
